83e950f70bf0196cfccb4fd79252002777b59518
24broken24 <144602433+24broken24@users.noreply.github.com>
2023-12-26 09:53:19 +0300

    Add files via upload

---
+++ individ3/src/Car.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+// Класс автомобиля
+class Car {
+    private String licensePlate;
+    private String brand;
+    private String model;
+    private int year;
+
+    public Car(String licensePlate, String brand, String model, int year) {
+        this.licensePlate = licensePlate;
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+    }
+
+    public String getLicensePlate() {
+        return licensePlate;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "licensePlate='" + licensePlate + '\'' +
+                ", brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", year=" + year +
+                '}';
+    }
+}
---
+++ individ3/src/Main.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Autopark autopark = new Autopark();
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Введите колличество машин:");
+        int n=sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            System.out.println("Введите информацию об автомобилях (гос. номер, марка, модель, год выпуска):");
+            System.out.print("Автомобиль " + (i + 1) + ": ");
+            String licensePlate = sc.next();
+            String brand = sc.next();
+            String model = sc.next();
+            int year = sc.nextInt();
+            Car car = new Car(licensePlate, brand, model, year);
+            autopark.addCar(car);
+        }
+
+        try {
+            // Удаление автомобиля
+            System.out.print("Введите государственный номер автомобиля для удаления: ");
+            String licensePlateToRemove = sc.next();
+            autopark.removeCar(licensePlateToRemove);
+            // Сортировка по году выпуска
+            autopark.sortCars(Comparator.comparingInt(Car::getYear));
+            // Поиск автомобиля
+            System.out.print("Введите государственный номер автомобиля для поиска: ");
+            String licensePlateToFind = sc.next();
+            Car foundCar = autopark.findCar(licensePlateToFind);
+            System.out.println("Найденный автомобиль: " + foundCar);
+
+        } catch (Autopark.CarNotFoundException e) {
+            System.out.println("Ошибка: " + e.getMessage());
+        }
+    }
+}
